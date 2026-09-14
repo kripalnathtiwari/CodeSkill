e342ed3da7237c1cfcd8dcf68e9a248a3c2784bc
kripalnathtiwari <145249601+kripalnathtiwari@users.noreply.github.com>
2026-09-14 14:11:26 +0530

    feat: prevent logged in users from accessing public website routes

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -7,6 +7,7 @@ import NetworkStatus from './components/NetworkStatus';
 import ProtectedRoute from './components/ProtectedRoute';
 import ScrollToTop from './components/ScrollToTop';
 import DashboardLayout from './components/dashboard/DashboardLayout';
+import GuestRoute from './components/GuestRoute';
 
 const Home = lazy(() => import('./pages/Home'));
 const Login = lazy(() => import('./pages/Login'));
@@ -64,13 +65,13 @@ function AppContent() {
         <ErrorBoundary>
           <Suspense fallback={<GlobalLoader />}>
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route path="/change-password" element={<ChangePassword />} />
-              <Route path="/sandbox" element={<Sandbox />} />
+              <Route path="/" element={<GuestRoute><Home /></GuestRoute>} />
+              <Route path="/about" element={<GuestRoute><About /></GuestRoute>} />
+              <Route path="/contact" element={<GuestRoute><Contact /></GuestRoute>} />
+              <Route path="/login" element={<GuestRoute><Login /></GuestRoute>} />
+              <Route path="/forgot-password" element={<GuestRoute><ForgotPassword /></GuestRoute>} />
+              <Route path="/change-password" element={<GuestRoute><ChangePassword /></GuestRoute>} />
+              <Route path="/sandbox" element={<GuestRoute><Sandbox /></GuestRoute>} />
               <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
               <Route path="/dashboard/courses" element={<ProtectedRoute><DashboardLayout><CoursesTraining /></DashboardLayout></ProtectedRoute>} />
               <Route path="/dashboard/practice" element={<ProtectedRoute><DashboardLayout><Problems /></DashboardLayout></ProtectedRoute>} />
@@ -88,39 +89,39 @@ function AppContent() {
               <Route path="/edit-profile" element={<ProtectedRoute><EditProfile /></ProtectedRoute>} />
               <Route path="/my-courses" element={<ProtectedRoute><MyCourses /></ProtectedRoute>} />
               <Route path="/dashboard/my-courses" element={<ProtectedRoute><DashboardLayout><MyCourses /></DashboardLayout></ProtectedRoute>} />
-              <Route path="/problems" element={<Problems />} />
-              <Route path="/problems/:tag" element={<TagProblems />} />
-              <Route path="/aptitude" element={<Aptitude />} />
-              <Route path="/aptitude/topic/:topic" element={<AptitudeTopic />} />
-              <Route path="/company-problems" element={<CompanyProblems />} />
-              <Route path="/test-series" element={<TestSeries />} />
-              <Route path="/courses-training" element={<CoursesTraining />} />
-              <Route path="/course/:id" element={<CourseDetails />} />
+              <Route path="/problems" element={<GuestRoute><Problems /></GuestRoute>} />
+              <Route path="/problems/:tag" element={<GuestRoute><TagProblems /></GuestRoute>} />
+              <Route path="/aptitude" element={<GuestRoute><Aptitude /></GuestRoute>} />
+              <Route path="/aptitude/topic/:topic" element={<GuestRoute><AptitudeTopic /></GuestRoute>} />
+              <Route path="/company-problems" element={<GuestRoute><CompanyProblems /></GuestRoute>} />
+              <Route path="/test-series" element={<GuestRoute><TestSeries /></GuestRoute>} />
+              <Route path="/courses-training" element={<GuestRoute><CoursesTraining /></GuestRoute>} />
+              <Route path="/course/:id" element={<GuestRoute><CourseDetails /></GuestRoute>} />
               <Route path="/dashboard/course/:id" element={<ProtectedRoute><DashboardLayout><CourseDetails /></DashboardLayout></ProtectedRoute>} />
               <Route path="/register/:id" element={<ProtectedRoute><CourseRegistration /></ProtectedRoute>} />
               <Route path="/dashboard/register/:id" element={<ProtectedRoute><DashboardLayout><CourseRegistration /></DashboardLayout></ProtectedRoute>} />
               <Route path="/payment/:id" element={<ProtectedRoute><Payment /></ProtectedRoute>} />
               <Route path="/dashboard/payment/:id" element={<ProtectedRoute><DashboardLayout><Payment /></DashboardLayout></ProtectedRoute>} />
               <Route path="/admin" element={<ProtectedRoute adminOnly><Admin /></ProtectedRoute>} />
-              <Route path="/contests" element={<Contests />} />
+              <Route path="/contests" element={<GuestRoute><Contests /></GuestRoute>} />
               <Route path="/take-test/:id" element={<ProtectedRoute><DashboardLayout><TakeTest /></DashboardLayout></ProtectedRoute>} />
               <Route path="/solve/:id" element={<ProtectedRoute><SolveProblem /></ProtectedRoute>} />
               <Route path="/dashboard/solve/:id" element={<ProtectedRoute><DashboardLayout><SolveProblem /></DashboardLayout></ProtectedRoute>} />
               <Route path="/aptitude/:id" element={<ProtectedRoute><SolveAptitude /></ProtectedRoute>} />
               <Route path="/dashboard/aptitude/:id" element={<ProtectedRoute><DashboardLayout><SolveAptitude /></DashboardLayout></ProtectedRoute>} />
               <Route path="/dashboard/aptitude/topic/:topic" element={<ProtectedRoute><DashboardLayout><AptitudeTopic /></DashboardLayout></ProtectedRoute>} />
-              <Route path="/other-practice" element={<OtherPractice />} />
-              <Route path="/other-practice/topic/:topic" element={<OtherPracticeTopic />} />
+              <Route path="/other-practice" element={<GuestRoute><OtherPractice /></GuestRoute>} />
+              <Route path="/other-practice/topic/:topic" element={<GuestRoute><OtherPracticeTopic /></GuestRoute>} />
               <Route path="/dashboard/other-practice/topic/:topic" element={<ProtectedRoute><DashboardLayout><OtherPracticeTopic /></DashboardLayout></ProtectedRoute>} />
               <Route path="/other-practice/solve/:id" element={<ProtectedRoute><SolveOtherPractice /></ProtectedRoute>} />
               <Route path="/dashboard/other-practice/solve/:id" element={<ProtectedRoute><DashboardLayout><SolveOtherPractice /></DashboardLayout></ProtectedRoute>} />
               <Route path="/verify" element={<VerifyCertificate />} />
-              <Route path="/cv-builder" element={<CVBuilder />} />
-              <Route path="/cv-templates" element={<CVTemplates />} />
+              <Route path="/cv-builder" element={<GuestRoute><CVBuilder /></GuestRoute>} />
+              <Route path="/cv-templates" element={<GuestRoute><CVTemplates /></GuestRoute>} />
               <Route path="/dashboard/career/cv-templates" element={<ProtectedRoute><DashboardLayout><CVTemplates /></DashboardLayout></ProtectedRoute>} />
-              <Route path="/ats-checker" element={<ATSChecker />} />
-              <Route path="/jobs" element={<Jobs />} />
-              <Route path="/our-product" element={<OurProduct />} />
+              <Route path="/ats-checker" element={<GuestRoute><ATSChecker /></GuestRoute>} />
+              <Route path="/jobs" element={<GuestRoute><Jobs /></GuestRoute>} />
+              <Route path="/our-product" element={<GuestRoute><OurProduct /></GuestRoute>} />
             </Routes>
           </Suspense>
         </ErrorBoundary>
